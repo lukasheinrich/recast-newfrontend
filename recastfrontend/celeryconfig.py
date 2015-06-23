34d07b23ff3c3651fe135050c1c1702e145db27f
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-06-23 18:26:57 -0400

    celery

--- recastfrontend/celeryconfig.py
+++ recastfrontend/celeryconfig.py
@@ -1,8 +1,8 @@
 from recastfrontend.frontendconfig import config as frontendconf
 
-BROKER_URL = 'redis://' + frontendconf['HOSTNAME']
-CELERY_RESULT_BACKEND = "redis"
-CELERY_REDIS_HOST = frontendconf['HOSTNAME']
-CELERY_REDIS_PORT = 6379
-CELERY_REDIS_DB = 0
-CELERY_TRACK_STARTED = True
+BROKER_URL = 'redis://redistogo:52cc7dc453b4af5617c9febf07a327b3@hammerjaw.redistogo.com:10755/'
+# CELERY_RESULT_BACKEND = "redis"
+# CELERY_REDIS_HOST = frontendconf['HOSTNAME']
+# CELERY_REDIS_PORT = 6379
+# CELERY_REDIS_DB = 0
+# CELERY_TRACK_STARTED = True
