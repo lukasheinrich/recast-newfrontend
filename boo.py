e83c32b17608314d6e2d82707158653689acbf5d
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-06-23 18:16:02 -0400

    Proc

--- boo.py
+++ boo.py
@@ -6,5 +6,7 @@ app = Flask('app')
 def home():
   return 'hello'
 
+import os
 if __name__ == '__main__':
-  app.run()
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
