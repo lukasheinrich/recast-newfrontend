0bccb6fe155f92e4138a7b439b3c5feb86155813
Christian <borachristia@gmail.com>
2016-02-22 15:35:35 -0600

    updating test cases

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,7 @@ from recastfrontend.server import db
 import recastdb.models
 from flask import url_for
 
+
 class FlaskClientTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app()
@@ -16,37 +17,44 @@ class FlaskClientTestCase(unittest.TestCase):
         
     def tearDown(self):
         db.session.remove()
-        #db.drop_all()
         self.app_context.pop()
+        
 
     def test_home_page(self):
         #response = self.client.get(url_for('./'))
         #self.assetTrue('Stranger' in response.get_data(as_text=True))
         #print self.client.get(url_for('about'))
-        #pass
         pass
 
-    def testUser(self):
+    def test_db(self):
         user = recastdb.models.User(name="Test User", email="test@email.com")
         db.session.add(user)
         db.session.commit()
-
-    def testAnalysis(self):
+        
         run_condition = recastdb.models.RunCondition(name="run condition test", description="test")
         db.session.add(run_condition)
         db.session.commit()
-        analysis = recastdb.models.Analysis(title="Analysis title test", description="Testing description test", collaboration="ATLAS", run_condition_id=run_condition.id, owner_id=1)
+        
+        analysis = recastdb.models.Analysis(title="Analysis title test", description="Testing description test", collaboration="ATLAS", run_condition_id=run_condition.id, owner_id=user.id)
         db.session.add(analysis)
         db.session.commit()
 
-    def testRequest(self):
-        pass
-        
+        request = recastdb.models.ScanRequest(description_of_model="Model description test", reason_for_request="Reason for request test", additional_information="Additional info test", analysis_id=analysis.id, requester_id=user.id)
+        db.session.add(request)
+        db.session.commit()
 
-    def testDB(self):
-        db.session.query(recastdb.models.User).all()
-        
-                                    
-        
-    def getClient(self):
-        return self.client
+        point_request = recastdb.models.PointRequest(scan_request_id=request.id, requester_id=user.id)
+        db.session.add(point_request)
+        db.session.commit()
+
+        basic_request = recastdb.models.BasicRequest(point_request_id=point_request.id, requester_id=user.id, number_of_events=0)
+        db.session.add(basic_request)
+        db.session.commit()
+
+        lhe_file = recastdb.models.LHEFile(basic_request_id=basic_request.id)
+        db.session.add(lhe_file)
+        db.session.commit()
+
+        subscription = recastdb.models.Subscription(subscription_type="Observer", description="Description test", requirements="Requirements test", notifications="Recast Requests", subscriber_id=user.id, analysis_id=analysis.id)
+        db.session.add(subscription)
+        db.session.commit()
