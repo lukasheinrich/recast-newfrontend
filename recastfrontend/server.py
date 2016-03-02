f1d2c24d8d261eb43d22a97dac88a4384cd4b3ee
Christian <borachristian@gmail.com>
2016-03-02 15:36:00 -0600

    debugging

--- recastfrontend/server.py
+++ recastfrontend/server.py
@@ -421,7 +421,7 @@ def profile():
 
   return render_template('profile.html', db_user = user_query[0], tokens=user_query[0].access_tokens)
 
-@app.route("/token", methods=['POST'])
+@app.route("/token", methods=['GET', 'POST'])
 @login.login_required
 def show_token():
   user_query = dbmodels.User.query.filter(dbmodels.User.name == login.current_user.name()).all()
